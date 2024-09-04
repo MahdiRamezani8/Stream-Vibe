adb94ea5ea79bb2257191167647d284c4a7cfb00
mahdi <ramezani86mraz@gmail.com>
2024-09-05 01:01:48 +0330

    (feature) FAQ (Frequently Asked Quetions) view implemnted

--- js/controlles/indexController.js
+++ js/controlles/indexController.js
@@ -3,13 +3,15 @@ import "regenerator-runtime/runtime";
 import NavView from "../views/sectionsView/navView";
 import GenreSplideView from "../views/sectionsView/genreSplideView";
 import DevicesView from "../views/indexViews/devicesView";
-import { devices } from "../config";
+import { devices, FAQs } from "../config";
+import FAQsView from "../views/sectionsView/FAQsView";
 
 function init() {
   // appending elements:
   GenreSplideView.render();
   NavView.render();
   DevicesView.render(devices);
+  FAQsView.render(FAQs);
 }
 
 init();
